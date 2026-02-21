d2b3a23fb8beddbee8ac847a6e9bc4c055a005e8
nithinreddy045 <reddynithin6777@gmail.com>
2026-02-21 21:36:09 +0530

    QuickKart backend ready

---
+++ src/main/java/com/example/demo/QuickKartApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickKartApplication.class, args);
+	}
+
+}
